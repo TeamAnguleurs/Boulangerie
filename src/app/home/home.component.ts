3de6ae7f22272a7a0d89cff33d539c0b0e2cf880
Davlemecreant <107454708+Davlemecreant@users.noreply.github.com>
2022-07-28 13:18:21 +0200

    Add files via upload
    Home page avec directive ngFor sur les 3 premières images + vidéo qui fonctionne

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,30 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  detoxslim = [
+    {
+      id: 1,
+      name: 'Démarrer une cure détox',
+      description: 'Minceur & Digestion',
+      image: '../../assets/img/accueil/detox-slim-fit.jpg',
+
+    },
+    { 
+      id: 2,
+      name: 'Sélection remise en forme',
+      description: "Arriver en forme pour l'été",
+      image: '../../assets/img/accueil/instagram-feed4_360x.jpg',
+
+    },
+    {
+      id: 3,
+      name: 'Tisanes pour les enfants',
+      description: 'Infusions à déguster en famille',
+      image: '../../assets/img/accueil/infusions_animaux_540x.jpg',
+
+    },
+ 
+  ];
 }
+
+
